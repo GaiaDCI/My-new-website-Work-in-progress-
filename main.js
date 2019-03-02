25ab9706cedc24499a2be212ee099507897007ed
Gaia Cicaloni <39329527+GaiaDCI@users.noreply.github.com>
2019-03-02 16:13:28 +0100

    link and structure updated

---
+++ main.js
@@ -0,0 +1,31 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const wrapper = document.querySelector(".wrapper");
+
+left.addEventListener('mouseenter', () => {
+    wrapper.classList.add('hover-left');
+});
+
+left.addEventListener('mouseleave', () => {
+    wrapper.classList.remove('hover-left');
+})
+
+right.addEventListener('mouseenter', () => {
+    wrapper.classList.add('hover-right');
+})
+
+right.addEventListener('mouseleave', () => {
+    wrapper.classList.remove('hover-right');
+})
+
+
+
+
+
+
+
+
+
+function Show(id) {
+    if (document.getElementById) document.getElementById(id).style.display = "block";
+}
